11d7096e56c89e4a9f04cfe37dff3e5b96678a5c
adam-kuhn <adam.kuhn20@gmail.com>
2018-05-31 16:39:10 +1200

    trying to update cal count as forms update

--- client/components/Lap.jsx
+++ client/components/Lap.jsx
@@ -10,15 +10,32 @@ class Lap extends React.Component {
       gu: 0,
       clifbar: 0,
       balls: 0,
-      pbj: 0
+      pbj: 0,
+      calories: 0
     }
     this.handleChange = this.handleChange.bind(this)
     this.submitLap = this.submitLap.bind(this)
   }
 
   handleChange (e) {
+    const {gu, clifbar, balls, pbj} = this.state
+    const amountSelected = gu + clifbar + balls + pbj
+    const previousCal = this.state.calories
+    // let updateCal = 0
+    // if (e.target.name === 'gu') {
+    //   updateCal += (e.target.value * 100)
+    // }
+    // if (e.target.name === 'clifbar') {
+    //   updateCal += (e.target.value * 50)
+    // }
+    // if (e.target.name === 'balls') {
+    //   updateCal += (e.target.value * 23)
+    // }
+    const itemsCal = e.target.getAttribute('data-cal')
+    const calories = itemsCal * e.target.value
     this.setState({
-      [e.target.name]: Number(e.target.value)
+      [e.target.name]: Number(e.target.value),
+      calories: previousCal + calories
     })
   }
 
@@ -34,7 +51,7 @@ class Lap extends React.Component {
         <p>Values below are ~100 calories each</p>
         <ul>
           <li>Gu (1 gel)
-            <input value={this.state.gu} name='gu' onChange={this.handleChange} />
+            <input value={this.state.gu} name='gu' onChange={this.handleChange} data-cal={100} />
           </li>
           <li>ClifBar (1/2)
             <input value={this.state.clifbar} name='clifbar' onChange={this.handleChange} />
@@ -46,6 +63,7 @@ class Lap extends React.Component {
             <input value={this.state.pbj} name='pbj' onChange={this.handleChange} />
           </li>
         </ul>
+        <h3>Calories {this.state.calories}</h3>
         <button type='button' onClick={this.submitLap}>Next Lap</button>
       </div>
     )
