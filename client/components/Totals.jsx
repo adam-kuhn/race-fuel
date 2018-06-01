40132e603316484e42db55fe9836ab920a493d03
adam-kuhn <adam.kuhn20@gmail.com>
2018-06-02 10:54:06 +1200

    re arrange order of totals list

--- client/components/Totals.jsx
+++ client/components/Totals.jsx
@@ -7,10 +7,10 @@ class Totals extends React.Component {
       <div>
         <h2>Totals!</h2>
         <p>Calories eaten {this.props.calories}</p>
-        <p>ClifBars eaten {this.props.clifbar}</p>
         <p>Gu gels eaten {this.props.gu}</p>
-        <p>Peanut Butter and Jelly Sandwiches eaten {this.props.pbj}</p>
+        <p>ClifBars eaten {this.props.clifbar}</p>
         <p>Power Balls eaten {this.props.balls}</p>
+        <p>Peanut Butter and Jelly Sandwiches eaten {this.props.pbj}</p>
       </div>
     )
   }
