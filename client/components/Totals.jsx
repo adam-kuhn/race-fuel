b5bc798a1443669323e235c844db7511af87a5f6
adam-kuhn <adam.kuhn20@gmail.com>
2018-06-04 11:07:35 +1200

    set up totals state to render with a map

--- client/components/Totals.jsx
+++ client/components/Totals.jsx
@@ -6,28 +6,18 @@ class Totals extends React.Component {
     return (
       <div>
         <h2>Totals!</h2>
-        <p>Calories eaten {this.props.calories}</p>
-        <p>Gu gels eaten {this.props.gu}</p>
-        <p>ClifBars eaten {this.props.clifbar}</p>
-        <p>Power Balls eaten {this.props.balls}</p>
-        <p>Peanut Butter and Jelly Sandwiches eaten {this.props.pbj}</p>
-        <p>Banana eaten {this.props.banana} </p>
-        <p>Water drank {this.props.water} L</p>
+        {this.props.fuel.map(item => {
+          return (
+            <p key={item.id}>{item.text} {item.amount}</p>
+          )
+        })}
       </div>
     )
   }
 }
 
 function mapStateToProps (state) {
-  return {
-    calories: state.totals.calories,
-    clifbar: state.totals.clifbar,
-    gu: state.totals.gu,
-    pbj: state.totals.pbj,
-    balls: state.totals.balls,
-    water: state.totals.water,
-    banana: state.totals.banana
-  }
+  return state.totals
 }
 
 export default connect(mapStateToProps)(Totals)
--- client/reducers/totals.js
+++ client/reducers/totals.js
@@ -1,13 +1,50 @@
 import {NEXT_LAP} from '../actions'
 
 const initialState = {
-  calories: 0,
-  clifbar: 0,
-  gu: 0,
-  pbj: 0,
-  balls: 0,
-  banana: 0,
-  water: 0
+  fuel: [
+    {
+      id: 1,
+      type: 'calories',
+      amount: 0,
+      text: 'Calories eaten'
+    },
+    {
+      id: 2,
+      type: 'clifbar',
+      amount: 0,
+      text: 'ClifBars eaten'
+    },
+    {
+      id: 3,
+      type: 'gu',
+      amount: 0,
+      text: 'Gu gels eaten'
+    },
+    {
+      id: 4,
+      type: 'pbj',
+      amount: 0,
+      text: 'Peanut Butter and Jelly Sandwiches eaten'
+    },
+    {
+      id: 5,
+      type: 'balls',
+      amount: 0,
+      text: 'Power Balls eaten'
+    },
+    {
+      id: 6,
+      type: 'banana',
+      amount: 0,
+      text: 'Bananas eaten'
+    },
+    {
+      id: 7,
+      type: 'water',
+      amount: 0,
+      text: 'Water drank'
+    }
+  ]
 }
 
 function totals (state = initialState, action) {
