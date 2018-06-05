8de56feeb1fea2a21a7284f86a5059f1cb394a0e
adam-kuhn <adam.kuhn20@gmail.com>
2018-06-05 14:06:03 +1200

    totals reducer removed

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,10 +2,8 @@ import {combineReducers} from 'redux'
 
 import fuelList from './fuelList'
 import display from './display'
-import totals from './totals'
 
 export default combineReducers({
   display,
-  fuelList,
-  // totals
+  fuelList
 })
