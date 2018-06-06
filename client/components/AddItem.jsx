d8a0cd6c1ade9a41cb58ca9c30bc74c73ce946d4
adam-kuhn <adam.kuhn20@gmail.com>
2018-06-06 13:31:40 +1200

    set up basic inputs

--- client/components/AddItem.jsx
+++ client/components/AddItem.jsx
@@ -10,6 +10,9 @@ class AddItem extends React.Component {
     return (
       <div>
         <h3>Add Custom Item</h3>
+        <p>Item: <input /> </p>
+        <p>Amount: <input /> </p>
+        <p>Calories per serving: <input /> </p>
       </div>
     )
   }
