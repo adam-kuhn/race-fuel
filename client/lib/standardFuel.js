398b8ae9197b19cb8cba7373e464dc73f5354eda
adam-kuhn <adam.kuhn20@gmail.com>
2018-06-06 16:30:23 +1200

    move starting fuel list to its own file

---
+++ client/lib/standardFuel.js
@@ -0,0 +1,68 @@
+export default
+[
+  {
+    id: 1,
+    name: 'calories',
+    totalAmount: 0,
+    totalText: 'Calories eaten'
+  },
+  {
+    id: 2,
+    name: 'clifbar',
+    amount: 0,
+    liveText: 'ClifBars',
+    lapText: 'ClifBars (1/2)',
+    totalAmount: 0,
+    totalText: 'ClifBars eaten',
+    itemCalories: 135
+  },
+  {
+    id: 3,
+    name: 'gu',
+    amount: 0,
+    liveText: 'Gu',
+    lapText: 'Gu (1 gel)',
+    totalAmount: 0,
+    totalText: 'Gu gels eaten',
+    itemCalories: 100
+  },
+  {
+    id: 4,
+    name: 'pbj',
+    amount: 0,
+    liveText: 'Peanut Butter and Jelly Sandwiches',
+    lapText: 'Peanut Butter and Jelly Sandwiches (1/2)',
+    totalAmount: 0,
+    totalText: 'Peanut Butter and Jelly Sandwiches eaten',
+    itemCalories: 100
+  },
+  {
+    id: 5,
+    name: 'balls',
+    amount: 0,
+    liveText: 'Power Balls',
+    lapText: 'Power Balls (2 balls)',
+    totalAmount: 0,
+    totalText: 'Power Balls eaten',
+    itemCalories: 110
+  },
+  {
+    id: 6,
+    name: 'banana',
+    amount: 0,
+    liveText: 'Bananas',
+    lapText: 'Bananas (1)',
+    totalAmount: 0,
+    totalText: 'Bananas eaten',
+    itemCalories: 100
+  },
+  {
+    id: 7,
+    name: 'water',
+    amount: 0,
+    liveText: 'Water (mL)',
+    lapText: 'Water (mL)',
+    totalAmount: 0,
+    totalText: 'Water drank (mL)'
+  }
+]
