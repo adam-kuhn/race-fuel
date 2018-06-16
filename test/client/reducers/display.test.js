e79f86aa1a6ebe2b4195b7acd8605e7a07fc0ccd
adam-kuhn <adam.kuhn20@gmail.com>
2018-06-16 14:04:12 +1200

    passing display tests on STANDARD

--- test/client/reducers/display.test.js
+++ test/client/reducers/display.test.js
@@ -13,6 +13,7 @@ test('display returns correct display on SHOW_FUEL_LIST', () => {
   const actual = display(state, action)
   expect(actual.showFuelList).toBe(expected)
 })
+
 test('display does not overwrite previous state on SHOW_FUEL_LIST', () => {
   const expected = 2
   const action = {
@@ -23,7 +24,7 @@ test('display does not overwrite previous state on SHOW_FUEL_LIST', () => {
   expect(keys.length).toBe(expected)
 })
 
-test('display return correct display on SETLIST', () => {
+test('display returns correct display on SETLIST', () => {
   const expected = false
   const action = {
     type: SETLIST
@@ -31,10 +32,30 @@ test('display return correct display on SETLIST', () => {
   const actual = display(state, action)
   expect(actual.showFuelList).toBe(expected)
 })
+
 test('display does not overwrite previous state on SETLIST', () => {
   const expected = 2
   const action = {
     type: SETLIST
+  }
+  const actual = display(state, action)
+  const keys = Object.keys(actual)
+  expect(keys.length).toBe(expected)
+})
+
+test('display returns correct display on STANDARD', () => {
+  const expected = false
+  const action = {
+    type: STANDARD
+  }
+  const actual = display(state, action)
+  expect(actual.showSelect).toBe(expected)
+})
+
+test('display does overwrite previous state on STANDARD', () => {
+  const expected = 1
+  const action = {
+    type: STANDARD
   }
   const actual = display(state, action)
   const keys = Object.keys(actual)
