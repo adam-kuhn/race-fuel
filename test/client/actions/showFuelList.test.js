4dae8e5792cc8ec165279d49d672d7a32948e1ac
adam-kuhn <adam.kuhn20@gmail.com>
2018-06-16 13:42:03 +1200

    passing showFuelList action test

--- test/client/actions/showFuelList.test.js
+++ test/client/actions/showFuelList.test.js
@@ -1,3 +1,7 @@
-test('test suit is running', () => {
-  expect(true).toBeTruthy()
+import {showFuelList, SHOW_FUEL_LIST} from '../../../client/actions'
+
+test('showFuelList dispatches correct action.type SHOW_FUEL_LIST', () => {
+  const expected = SHOW_FUEL_LIST
+  const actual = showFuelList()
+  expect(actual.type).toBe(expected)
 })
