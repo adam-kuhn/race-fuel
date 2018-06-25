bdd3c8a716ca8e6c191568361b5f6b6c8cd11d88
adam-kuhn <adam.kuhn20@gmail.com>
2018-06-25 20:09:40 +1200

    toggle text of distance total when button is clicked

--- client/components/MeasurementTotal.jsx
+++ client/components/MeasurementTotal.jsx
@@ -4,15 +4,24 @@ import {connect} from 'react-redux'
 const MeasurementTotal = (props) => {
   return (
     props.measurements.map(unit => {
-      return (
-        <p className="card-text" key={unit.id}>{unit.totalTextKm} {unit.totalAmount}</p>
-      )
+      if (props.km && unit.name === 'distance') {
+        return (
+          <p className="card-text" key={unit.id}>{unit.totalTextKm} {unit.totalAmount}</p>
+        )
+      } else {
+        return (
+          <p className="card-text" key={unit.id}>{unit.totalTextMi || unit.totalText} {unit.totalAmount}</p>
+        )
+      }
     })
   )
 }
 
 const mapStateToProps = (state) => {
-  return state.distanceTime
+  return {
+    measurements: state.distanceTime.measurements,
+    km: state.display.km
+  }
 }
 
 export default connect(mapStateToProps)(MeasurementTotal)
