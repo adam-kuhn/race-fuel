e01e52b72391a88c01f94c98aa9199f446c9b756
adam-kuhn <adam.kuhn20@gmail.com>
2018-07-09 09:13:23 +0300

    style time input

--- client/components/Lap/Measurements/LapMeasurements/LapTime.jsx
+++ client/components/Lap/Measurements/LapMeasurements/LapTime.jsx
@@ -3,20 +3,30 @@ import React from 'react'
 const LapTime = (props) => {
   const {text, value, change} = props
   return (
-    <p className="card-text">
-      {text.main} {text.hour}
-      <input className="form-control" name={text.hour}
-        value={value[text.hour] || ''} onChange={change}
-        placeholder="0"/>
-      {text.min}
-      <input className="form-control" name={text.min}
-        value={value[text.min] || ''} onChange={change}
-        placeholder="0"/>
-      {text.sec}
-      <input className="form-control" name={text.sec}
-        value={value[text.sec] || ''} onChange={change}
-        placeholder="0"/>
-    </p>
+    <div>
+      <p className="card-text">
+        {text.main}:
+      </p>
+      <div className='time'>
+        <p>{text.hour}
+          <input className="form-control time-input" name={text.hour}
+            value={value[text.hour] || ''} onChange={change}
+            placeholder="0"/>
+        </p>
+        <p>
+          {text.min}
+          <input className="form-control time-input" name={text.min}
+            value={value[text.min] || ''} onChange={change}
+            placeholder="0"/>
+        </p>
+        <p>
+          {text.sec}
+          <input className="form-control time-input" name={text.sec}
+            value={value[text.sec] || ''} onChange={change}
+            placeholder="0"/>
+        </p>
+      </div>
+    </div>
   )
 }
 
