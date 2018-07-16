16bd53122ac0aa895d629e38b81db78b899ccc85
adam-kuhn <adam.kuhn20@gmail.com>
2018-07-16 11:44:31 +0300

    able to display keyName as text and corresponding info

--- client/components/Splits/LapInfo/LapInfo.jsx
+++ client/components/Splits/LapInfo/LapInfo.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const LapInfo = (props) => {
+  const keys = Object.keys(props.info)
+  const data = keys.map((keyName, idx) => {
+    if (keyName !== 'time') {
+      return (
+        <p key={idx}>{keyName} {props.info[keyName]} </p>
+      )
+    }
+  })
   return (
     <div>
-      <p>Lap Info</p>
+      {data}
     </div>
   )
 }
