c96569421fa956027e2d2a9f04f74307b075d6d1
adam-kuhn <adam.kuhn20@gmail.com>
2018-07-17 17:05:41 +0300

    move nav functions to one function

--- client/components/Splits/SplitNav/SplitNav.jsx
+++ client/components/Splits/SplitNav/SplitNav.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 const SplitNav = (props) => {
-  const {errorMsg, next, previous} = props
+  const {errorMsg, next, previous, nav} = props
   return (
     <div>
       <p>{errorMsg}</p>
-      <button type='button' onClick={previous}>Previous</button>
-      <button type='button' onClick={next}>Next</button>
+      {/* <button type='button' onClick={previous} value={-1}>Previous</button>
+      <button type='button' onClick={next} value={1}>Next</button> */}
+      <button type='button' onClick={nav} value={-1}>SplitPrevious</button>
+      <button type='button' onClick={nav} value={1}>Next</button>
     </div>
   )
 }
