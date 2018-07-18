b04f66e1103bebc80589014cd7f8b600eb5e3bb8
adam-kuhn <adam.kuhn20@gmail.com>
2018-07-18 08:26:50 +0300

    change switch statement to if..

--- client/components/Splits/SplitNav/SplitNav.jsx
+++ client/components/Splits/SplitNav/SplitNav.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 
 const SplitNav = (props) => {
-  const {errorMsg, next, previous, nav} = props
+  const {errorMsg, nav} = props
   return (
     <div>
       <p>{errorMsg}</p>
-      {/* <button type='button' onClick={previous} value={-1}>Previous</button>
-      <button type='button' onClick={next} value={1}>Next</button> */}
       <button type='button' onClick={nav} value={-1}>SplitPrevious</button>
       <button type='button' onClick={nav} value={1}>Next</button>
     </div>
