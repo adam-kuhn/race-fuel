dc09e7b0673a1501f3f6ba0d216d17291fc2a674
adam-kuhn <adam.kuhn20@gmail.com>
2018-07-22 20:08:50 +0300

    make split into a basic card

--- client/components/Splits/LapInfo/LapInfo.jsx
+++ client/components/Splits/LapInfo/LapInfo.jsx
@@ -5,11 +5,11 @@ const LapInfo = (props) => {
   const data = keys.map((keyName, idx) => {
     if (keyName !== 'time') {
       return (
-        <p key={idx}>{keyName} {props.info[keyName]} </p>
+        <p className='card-text' key={idx}>{keyName} {props.info[keyName]} </p>
       )
     } else {
       return (
-        <p key={idx}>{keyName} {props.info[keyName].hour || 0}hr {props.info[keyName].min || 0}min {props.info[keyName].sec || 0}s</p>
+        <p className='card-text' key={idx}>{keyName} {props.info[keyName].hour || 0}hr {props.info[keyName].min || 0}min {props.info[keyName].sec || 0}s</p>
       )
     }
   })
