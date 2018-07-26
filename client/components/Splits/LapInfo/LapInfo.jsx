b92365524585b8a858ede469079acd34140798fd
adam-kuhn <adam.kuhn20@gmail.com>
2018-07-26 08:50:03 -0600

    display splits correctly with new data

--- client/components/Splits/LapInfo/LapInfo.jsx
+++ client/components/Splits/LapInfo/LapInfo.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 const LapInfo = (props) => {
   const keys = Object.keys(props.info)
-  const data = keys.map((keyName, idx) => {
-    if (keyName !== 'time') {
+  console.log(keys)
+  const data = keys.map((item, idx) => {
+    if (item !== 'time') {
       return (
-        <p className='card-text' key={idx}>{keyName} {props.info[keyName]} </p>
+        <p className='card-text' key={idx}>{props.info[item].text} {props.info[item].value} </p>
       )
     } else {
       return (
-        <p className='card-text' key={idx}>{keyName} {props.info[keyName].hour || 0}hr {props.info[keyName].min || 0}min {props.info[keyName].sec || 0}s</p>
+        <p className='card-text' key={idx}>Time {props.info[item].hour.value || 0}hr {props.info[item].min.value || 0}min {props.info[item].sec.value || 0}s</p>
       )
     }
   })
