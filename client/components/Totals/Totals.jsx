1f89b50a40da7147e52b764231ed08a5a666d1c5
adam-kuhn <adam.kuhn20@gmail.com>
2018-08-03 12:37:23 -0700

    add distance select to Total component

--- client/components/Totals/Totals.jsx
+++ client/components/Totals/Totals.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
-import MeasurementTotal from './Measurements/MeasurementTotal'
 import LitreMlSelect from '../Select/UnitSelect/LitreMlSelect'
+import DistanceSelect from '../Select/UnitSelect/DistanceSelect'
+import MeasurementTotal from './Measurements/MeasurementTotal'
 
 class Totals extends React.Component {
   render () {
     return (
       <div className="card text-white bg-info mb-3">
         <h2 className="card-header">Totals!</h2>
+        <DistanceSelect />
         <LitreMlSelect />
         <div className="card-body">
           {this.props.fuelList.fuel.map(item => {
