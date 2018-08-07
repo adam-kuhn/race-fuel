ff7ea238c8e1592dd136ee583d174e9063e33bff
adam-kuhn <adam.kuhn20@gmail.com>
2018-08-07 15:55:30 -0600

    remove unused action types

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,9 +1,6 @@
 export const SHOW_EDIT_LIST = 'SHOW_EDIT_LIST'
 export const GO_HOME = 'GO_HOME'
 export const SHOW_SPLITS = 'SHOW_SPLITS'
-export const SHOW_LAP = 'SHOW_LAP'
-export const SHOW_LIST = 'SHOW_LIST'
-export const SHOW_TOTALS = 'SHOW_TOTALS'
 
 export const showEditList = () => {
   return {
