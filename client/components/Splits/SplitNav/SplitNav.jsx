578f548e1792de2e20040608fcea5ceb0506fbaa
adam-kuhn <adam.kuhn20@gmail.com>
2018-08-08 17:28:10 -0600

    font awesome working corectly with nav buttons

--- client/components/Splits/SplitNav/SplitNav.jsx
+++ client/components/Splits/SplitNav/SplitNav.jsx
@@ -5,11 +5,11 @@ const SplitNav = (props) => {
   return (
     <div>
       <p className='split-info'>{errorMsg}</p>
-      <button type='button' onClick={nav} value={-1}> back
-        {/* <i className="far fa-arrow-alt-circle-left" onClick={nav} value={-1}></i> */}
+      <button type='button' onClick={nav} value={-1}
+        className="far fa-arrow-alt-circle-left">
       </button>
-      <button type='button' onClick={nav} value={1}>next
-        {/* <i className="far fa-arrow-alt-circle-right"></i> */}
+      <button type='button' onClick={nav} value={1}
+        className="far fa-arrow-alt-circle-right">
       </button>
     </div>
   )
