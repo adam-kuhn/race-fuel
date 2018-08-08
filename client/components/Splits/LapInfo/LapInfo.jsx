0d825f9ec21cfc631ff909194a8beccc686ea8b0
adam-kuhn <adam.kuhn20@gmail.com>
2018-08-08 17:25:15 -0600

    only showing 1 time in splits

--- client/components/Splits/LapInfo/LapInfo.jsx
+++ client/components/Splits/LapInfo/LapInfo.jsx
@@ -6,14 +6,16 @@ const LapInfo = (props) => {
     <div>
       <p>Lap {props.info.lap}</p>
       {keys.map((item, idx) => {
-        if (item !== 'time' && item !== 'lap') {
-          return (
-            <p className='card-text' key={idx}>{props.info[item].text} {props.info[item].value} </p>
-          )
-        } else {
-          return (
-            <p className='card-text' key={idx}>Time {props.info[item].hour || 0}hr {props.info[item].min || 0}min {props.info[item].sec || 0}s</p>
-          )
+        if (item !== 'lap') {
+          if (item !== 'time') {
+            return (
+              <p className='card-text' key={idx}>{props.info[item].text} {props.info[item].value} </p>
+            )
+          } else {
+            return (
+              <p className='card-text' key={idx}>Time {props.info[item].hour || 0}hr {props.info[item].min || 0}min {props.info[item].sec || 0}s</p>
+            )
+          }
         }
       })}
     </div>
--- client/components/Splits/SplitNav/SplitNav.jsx
+++ client/components/Splits/SplitNav/SplitNav.jsx
@@ -5,9 +5,11 @@ const SplitNav = (props) => {
   return (
     <div>
       <p className='split-info'>{errorMsg}</p>
-      <button type='button' onClick={nav} value={-1}><i className="far fa-arrow-alt-circle-left"></i></button>
-      <button type='button' onClick={nav} value={1}>
-        <i className="far fa-arrow-alt-circle-right"></i>
+      <button type='button' onClick={nav} value={-1}> back
+        {/* <i className="far fa-arrow-alt-circle-left" onClick={nav} value={-1}></i> */}
+      </button>
+      <button type='button' onClick={nav} value={1}>next
+        {/* <i className="far fa-arrow-alt-circle-right"></i> */}
       </button>
     </div>
   )
