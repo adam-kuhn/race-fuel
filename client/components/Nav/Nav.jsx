5a3b6a174620920f28c71efd44416d9ee437c86f
adam-kuhn <adam.kuhn20@gmail.com>
2018-09-03 10:03:15 +0300

    nav change from class based component to functional component

--- client/components/Nav/Nav.jsx
+++ client/components/Nav/Nav.jsx
@@ -3,22 +3,20 @@ import {connect} from 'react-redux'
 
 import GoTo from './GoTo/GoTo'
 
-class Nav extends React.Component {
-  render () {
-    return (
-      <div className='center width nav'>
-        <GoTo route='/RemainingFuel'
-          text='Fuel List' />
-        <GoTo route='LapDetails'
-          text='Lap Details' />
-        <GoTo route='/Totals'
-          text='Totals' />
-        <GoTo route='/Splits'
-          text='Splits'
-          disable={this.props.disableSplitButton} />
-      </div>
-    )
-  }
+const Nav = (props) => {
+  return (
+    <div className='center width nav'>
+      <GoTo route='/RemainingFuel'
+        text='Fuel List' />
+      <GoTo route='LapDetails'
+        text='Lap Details' />
+      <GoTo route='/Totals'
+        text='Totals' />
+      <GoTo route='/Splits'
+        text='Splits'
+        disable={props.disableSplitButton} />
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
