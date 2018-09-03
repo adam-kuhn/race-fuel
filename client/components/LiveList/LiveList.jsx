964f156901f3494b3109f88318b1472bcabb339f
adam-kuhn <adam.kuhn20@gmail.com>
2018-09-03 10:10:45 +0300

    livelist to functional component

--- client/components/LiveList/LiveList.jsx
+++ client/components/LiveList/LiveList.jsx
@@ -4,41 +4,39 @@ import {Link} from 'react-router-dom'
 
 import LitreMlSelect from '../Select/UnitSelect/LitreMlSelect'
 
-class LiveList extends React.Component {
-  render () {
-    return (
-      <div className='card width text-white bg-primary mb-3'>
-        <h3 className='card-header'>Remaining Fuel</h3>
-        <div className='toggle'>
-          <p>Units</p>
-          <LitreMlSelect />
-        </div>
-        <div className='card-body'>
-          {this.props.fuel.map(item => {
-            if (item.name !== 'calories') {
-              if (item.name === 'water') {
-                return (
-                  <p className="card-text" key={item.id}>{this.props.litre
-                    ? item.text.waterL : item.text.waterMl} {item.amount}</p>
-                )
-              }
+const LiveList = (props) => {
+  return (
+    <div className='card width text-white bg-primary mb-3'>
+      <h3 className='card-header'>Remaining Fuel</h3>
+      <div className='toggle'>
+        <p>Units</p>
+        <LitreMlSelect />
+      </div>
+      <div className='card-body'>
+        {props.fuel.map(item => {
+          if (item.name !== 'calories') {
+            if (item.name === 'water') {
               return (
-                <p className='card-text' key={item.id}>
-                  {item.text} {item.amount}</p>
+                <p className="card-text" key={item.id}>{props.litre
+                  ? item.text.waterL : item.text.waterMl} {item.amount}</p>
               )
             }
-          })}
-          <div className="center">
-            <Link to='/EditList'>
-              <button type='button' className="btn btn-primary btn-primary-card">
+            return (
+              <p className='card-text' key={item.id}>
+                {item.text} {item.amount}</p>
+            )
+          }
+        })}
+        <div className="center">
+          <Link to='/EditList'>
+            <button type='button' className="btn btn-primary btn-primary-card">
               Edit Fuel List</button>
-            </Link>
+          </Link>
 
-          </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 function mapStateToProps (state) {
