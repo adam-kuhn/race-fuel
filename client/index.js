241c6f0e15fbf343f6153b792517bf92c827c264
adam-kuhn <adam.kuhn20@gmail.com>
2018-09-06 11:41:48 +0300

    pull down changes

--- client/index.js
+++ client/index.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {Provider} from 'react-redux'
-import {createStore, applyMiddleware, compose} from 'redux'
-import thunkMiddleware from 'redux-thunk'
+import {createStore} from 'redux'
 import {persistStore} from 'redux-persist'
 import {PersistGate} from 'redux-persist/integration/react'
 
 import reducers from './reducers'
 import App from './components/App'
 
-let store = createStore(reducers, compose(
-  applyMiddleware(thunkMiddleware),
-  window.devToolsExtension ? window.devToolsExtension() : f => f
-))
+let store = createStore(reducers,
+  window.devToolsExtension ? window.devToolsExtension() : f => f)
 
 let persistor = persistStore(store)
 
