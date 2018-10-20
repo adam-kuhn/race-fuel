0d89a98cb87d79d53a819c96ae0abfae03ba4216
adam-kuhn <adam.kuhn20@gmail.com>
2018-10-20 15:23:39 +0300

    conver App to functional component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import {connect} from 'react-redux'
 import {HashRouter as Router, Route} from 'react-router-dom'
 
 import LandingPage from './LandingPage/LandingPage'
@@ -7,22 +6,20 @@ import LandingPage from './LandingPage/LandingPage'
 import Auth from './Auth/Auth'
 import LiveApp from './LiveApp/LiveApp'
 
-class App extends React.Component {
-  render () {
-    return (
-      <Router>
-        <div className='main-container'>
-          <div className="center title">
-            <h2>Race Fuel</h2>
-          </div>
-          <Route exact path="/" component={LandingPage} />
-          <Auth />
-          <LiveApp />
+const App = () => {
+  return (
+    <Router>
+      <div className='main-container'>
+        <div className="center title">
+          <h2>Race Fuel</h2>
         </div>
-      </Router>
+        <Route exact path="/" component={LandingPage} />
+        <Auth />
+        <LiveApp />
+      </div>
+    </Router>
 
-    )
-  }
+  )
 }
 
-export default connect()(App)
+export default App
