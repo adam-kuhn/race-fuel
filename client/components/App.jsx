49603c959a45553b64cf9dc51f270e1a50abd080
adam-kuhn <adam.kuhn20@gmail.com>
2019-10-20 15:51:47 +0300

    add username/guest user to heading to show who is signed in

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import {HashRouter as Router} from 'react-router-dom'
+import {connect} from 'react-redux'
 
 import LogOut from './Auth/LogOut/LogOut'
 import RouteLayout from '../Layout/RouteLayout'
 
 const App = (props) => {
+  const subTitle = props.userName ? props.userName : 'Guest User'
   return (
     <Router>
       <div>
         <div className='main-container'>
           <div className="center title">
             <h2>Race Fuel</h2>
+            <h5>{subTitle}</h5>
           </div>
         </div>
         <RouteLayout />
@@ -21,4 +24,6 @@ const App = (props) => {
   )
 }
 
-export default App
+const mapStateToProps = (state) => ({username: state.auth.userInfo.username})
+
+export default connect(mapStateToProps)(App)
